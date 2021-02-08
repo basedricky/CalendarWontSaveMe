8857f317cc250d04219f1df591d3980445c2ef90
basedricky <ricky@dragonflyut.com>
2021-02-07 18:55:19 -0700

    added dynamic styling depending on time of day

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
+// Setting the variable and using Moment to show the current date and time at the top of the page on page load.
 var now = moment().format('LLLL');
 console.log(now);
 
-let timeBlocks = {
-    "8 AM": "",
-    "9 AM": "",
-    "10 AM": "",
-    "11 AM": "",
-    "12 PM": "",
-    "1 PM": "",
-    "2 PM": "",
-    "3 PM": "",
-    "4 PM": "",
-    "5 PM": "",
-};
-
 function pageLoad() {
     document.getElementById("currentDay").innerHTML = now;
+};
 
+pageLoad();
+
+//local storage you're gonna to set it as setItem(time, toDO)
+
+//getItem
+
+//save event button onClick requires setItem
+
+
+var timer = setInterval(scheduler, 100000)
+
+function scheduler() {
+
+    var timeRows = document.querySelectorAll('.time-row');
+    console.log(timeRows)
+
+    var currentTime = moment().format('HH');
+    console.log(currentTime)
+
+    $(".time-row").each(function () {
+
+        var timeBlock = $(this).attr("id")
+
+        console.log(timeBlock)
+
+        //compare time here and add the class present, past, or future depending on case
+        //look up addClass and removeClass
+
+        if (timeBlock < currentTime) {
+            $(this).removeClass("present future")
+            $(this).addClass("past hour")
+
+        } else if (timeBlock > currentTime) {
+            $(this).removeClass("past present")
+            $(this).addClass("future hour")
+
+        } else {
+            $(this).removeClass("future past")
+            $(this).addClass("present hour")
+        }
+    })
 
 
 }
+scheduler();
 
-pageLoad();
+// pull current storage append to it, then push it again! for local storage. it will just overright it, otherwise.
